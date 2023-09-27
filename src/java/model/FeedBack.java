237838a42d9dce8a55cdd22de4ac8369e6b334f7
dt2401 <quanphse161574@fpt.edu.vn>
2023-09-27 14:12:19 +0700

    feedback

---
+++ src/java/model/FeedBack.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author QUANHONG
+ */
+public class FeedBack {
+    private String userID;
+    private String fullName;
+    private String rating;
+    private String comment;
+    private String productID;
+
+    public FeedBack() {
+    }
+
+    public FeedBack(String userID, String fullName, String rating, String comment, String productID) {
+        this.userID = userID;
+        this.fullName = fullName;
+        this.rating = rating;
+        this.comment = comment;
+        this.productID = productID;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+    
+}
