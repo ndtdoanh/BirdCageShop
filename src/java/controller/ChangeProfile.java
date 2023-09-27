04f91e3e3db7c8f4eb493f034582857f3fb69e24
tvmmanh/ADMIN <tivip1216vn@gmail.com>
2023-09-27 23:17:12 +0700

    Profile User

---
+++ src/java/controller/ChangeProfile.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import dao.UserDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.User;
+
+/**
+ *
+ * @author Admin
+ */
+@WebServlet(name = "ChangeProfile", urlPatterns = {"/changeProfile"})
+public class ChangeProfile extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String userID = request.getParameter("userID");
+        UserDAO dao = new UserDAO();
+        User user = dao.getUserByuserID(userID);
+        request.setAttribute("us", user);
+        request.getRequestDispatcher("profile.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            String userID = request.getParameter("userID");
+            String fullName = request.getParameter("fullName");
+            String password = request.getParameter("password");
+            String phone = request.getParameter("phone");
+            String email = request.getParameter("email");
+            String address = request.getParameter("address");
+            String roleID = request.getParameter("roleID");
+            UserDAO dao = new UserDAO();
+            dao.updateUser(userID, fullName, password, phone, email, address, roleID);
+            response.sendRedirect("homePage.jsp");
+        } catch (Exception e) {
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
