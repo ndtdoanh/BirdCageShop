8531f0834816e2bf204fab80423b14d9e7d28d2c
dt2401 <quanphse161574@fpt.edu.vn>
2023-10-18 01:52:57 +0700

    order request

---
+++ src/java/controller/OrderRequest.java
@@ -0,0 +1,167 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dao.MaterialDAO;
+import dao.OrderDAO;
+import dao.ProductDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Random;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Material;
+import model.User;
+
+/**
+ *
+ * @author trand
+ */
+@WebServlet(name = "OrderRequest", urlPatterns = {"/OrderRequest"})
+public class OrderRequest extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet OrderRequest</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet OrderRequest at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        MaterialDAO dao = new MaterialDAO();
+        List<Material> list = dao.getAllMaterial();
+        request.setAttribute("listM", list);
+        request.getRequestDispatcher("orderRequest.jsp").
+                forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+//            String cageType = request.getParameter("cageType");
+//            int pillar = Integer.parseInt(request.getParameter("pillar"));
+//            int hanger = Integer.parseInt(request.getParameter("hanger"));
+//            int spokes = Integer.parseInt(request.getParameter("spokes"));
+//            int bottom = Integer.parseInt(request.getParameter("bottom"));
+//            int door = Integer.parseInt(request.getParameter("door"));
+//            int cup = Integer.parseInt(request.getParameter("cup"));
+            HttpSession session = request.getSession();
+            int cageQuantity = Integer.parseInt(request.getParameter("cageQuantity"));
+            String description = request.getParameter("description");
+            double totalPrice = Double.parseDouble(request.getParameter("totalPrice"));
+            double shipCost = Double.parseDouble(request.getParameter("shipCost"));
+
+            MaterialDAO md = new MaterialDAO();
+            List<Material> list = md.getAllMaterial();
+
+            int length = 7;
+            Random random = new Random();
+            char[] text = new char[length];
+
+            for (int i = 0; i < length; i++) {
+                text[i] = (char) (random.nextInt(26) + 'a');
+            }
+
+            String cageID = new String(text);
+            ProductDAO dao = new ProductDAO();
+            OrderDAO od = new OrderDAO();
+
+            dao.insertProductRequest(cageID, "1", "CUSTOM CAGE", description, 0, 0, "", 0, "1");
+            int length1 = 4;
+            Random random1 = new Random();
+            char[] text1 = new char[length1];
+            for (int i = 0; i < length1; i++) {
+                text1[i] = (char) (random1.nextInt(26) + 'a');
+            }
+            String orderID = new String(text1);
+
+            User u = (User) session.getAttribute("LOGIN_USER");
+            java.util.Date currentDate = new java.util.Date();
+            java.sql.Date orderDate = new java.sql.Date(currentDate.getTime());
+            od.insertOrder(orderID, u.getUserID(), u.getPhone(), u.getAddress(), orderDate, "", "", shipCost, totalPrice);
+            od.insertOrderDetail(orderID, cageID, "CUSTOM CAGE", String.valueOf(totalPrice), cageQuantity);
+
+//            for (Material material : list) {
+//                if (material.getMaterialName().equals(cageType)) {
+//                    dao.insertMaterialRequest(cageID, material.getMaterialID(), cageQuantity, "");
+//                } else if (material.getMaterialName().equals("pillar")) {
+//                    dao.insertMaterialRequest(cageID, material.getMaterialID(), pillar, "");
+//                } else if (material.getMaterialName().equals("hanger")) {
+//                    dao.insertMaterialRequest(cageID, material.getMaterialID(), hanger, "");
+//                } else if (material.getMaterialName().equals("spokes")) {
+//                    dao.insertMaterialRequest(cageID, material.getMaterialID(), spokes, "");
+//                } else if (material.getMaterialName().equals("bottom")) {
+//                    dao.insertMaterialRequest(cageID, material.getMaterialID(), bottom, "");
+//                } else if (material.getMaterialName().equals("door")) {
+//                    dao.insertMaterialRequest(cageID, material.getMaterialID(), door, "");
+//                } else if (material.getMaterialName().equals("cup")) {
+//                    dao.insertMaterialRequest(cageID, material.getMaterialID(), cup, "");
+//                }
+//            }
+            //dao.insertMaterialRequest(cageID, "4", pillar, description);
+            //od.insertOrder(cageID, cageID, length, cageID, orderDate, cageID, description);
+            request.getRequestDispatcher("SuccessOrder.jsp").
+                    forward(request, response);
+        } catch (Exception e) {
+            request.getRequestDispatcher("Error.jsp").
+                    forward(request, response);
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
