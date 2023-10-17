ec4ec553d6217f55b19f6b7e088f49ba5cc66075
dt2401 <quanphse161574@fpt.edu.vn>
2023-10-18 01:55:20 +0700

    material model

---
+++ src/java/model/Material.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author trand
+ */
+public class Material {
+    String materialID;
+    String materialName;
+    String description;
+    String image;
+    double price;
+
+    public Material() {
+    }
+
+    public Material(String materialID, String materialName, String description, String image, double price) {
+        this.materialID = materialID;
+        this.materialName = materialName;
+        this.description = description;
+        this.image = image;
+        this.price = price;
+    }
+
+    public String getMaterialID() {
+        return materialID;
+    }
+
+    public void setMaterialID(String materialID) {
+        this.materialID = materialID;
+    }
+
+    public String getMaterialName() {
+        return materialName;
+    }
+
+    public void setMaterialName(String materialName) {
+        this.materialName = materialName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    
+}
