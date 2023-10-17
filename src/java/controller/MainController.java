494a988c70174813c2052309bdaed1708c6ac54a
TRONGDOANH\Acer <nguyentrongdoanh@gmail.com>
2023-10-18 01:02:05 +0700

    update maincontroller

--- src/java/controller/MainController.java
+++ src/java/controller/MainController.java
@@ -5,6 +5,7 @@
  */
 package controller;
 
+import dao.ProductDAO;
 import dao.UserDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -13,6 +14,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Category;
+import model.ProductDTO;
 import model.User;
 
 /**
@@ -20,72 +23,68 @@ import model.User;
  * @author HOANGDUC
  */
 public class MainController extends HttpServlet {
-    
+
     private static final String HOMEPAGE = "homePage.jsp";
-    
+
     private static final String LOGIN = "Login";
     private static final String LOGIN_CONTROLLER = "LoginController";
     private static final String LOGINPAGE = "LoginPage";
     private static final String LOGIN_PAGE = "login.jsp";
-    
+
     private static final String SEARCHUSER = "Search";
     private static final String SEARCHUSER_CONTROLLLER = "SearchUserController";
-    
+
     private static final String DELETEUSER = "deleteUser";
     private static final String DELETEUSER_CONTROLLER = "DeleteUserController";
-    
+
     private static final String UPDATEUSER = "updateUser";
     private static final String UPDATEUSER_CONTROLLLER = "UpdateUserController";
 
     private static final String LOGOUT = "Logout";
     private static final String LOGOUT_CONTROLLER = "LogoutController";
-    
+
     private static final String REGISTRATION = "Register";
     private static final String REGISTRATION_CONTROLLER = "RegistrationController";
-    
+
     private static final String ADDCONTROL = "AddControl";
     private static final String ADD_CONTROL = "AddControl";
-    
+
     private static final String UPDATECONTROL = "UpdateControl";
     private static final String UPDATE_CONTROL = "UpdateControl";
-    
+
     private static final String DELETECONTROL = "DeleteControl";
     private static final String DELETE_CONTROL = "DeleteControl";
-    
+
     private static final String LOADCONTROL = "LoadControl";
     private static final String LOAD_CONTROL = "LoadControl";
-    
-    
-    
-    
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            response.setContentType("text/html;charset=UTF-8");
-            String url = HOMEPAGE;
-            UserDAO dao = new UserDAO();
-            try {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = HOMEPAGE;
+        UserDAO dao = new UserDAO();
+        try {
             String action = request.getParameter("action");
             if (action == null) {
                 url = HOMEPAGE;
-            }
-            else if(SEARCHUSER.equals(action)){
+            } else if (SEARCHUSER.equals(action)) {
                 url = SEARCHUSER_CONTROLLLER;
-            }else if(UPDATEUSER.equals(action)){
+            } else if (UPDATEUSER.equals(action)) {
                 url = UPDATEUSER_CONTROLLLER;
-            }else if (LOGIN.equals(action)) {
+            } else if (LOGIN.equals(action)) {
                 url = LOGIN_CONTROLLER;
-            }  else if (REGISTRATION.equals(action)) {
+            } else if (REGISTRATION.equals(action)) {
                 url = REGISTRATION_CONTROLLER;
             } else if (LOGOUT.equals(action)) {
                 url = LOGOUT_CONTROLLER;
             } else if (LOGINPAGE.equals(action)) {
                 url = LOGIN_PAGE;
-            } 
-             
-        } catch (Exception e) {
-        }finally{
-                request.getRequestDispatcher(url).forward(request, response);
             }
+
+        } catch (Exception e) {
+        } finally {
+            request.getRequestDispatcher(url).forward(request, response);
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -100,7 +99,17 @@ public class MainController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        ProductDAO dao = new ProductDAO();
+        
+        List<ProductDTO> list = dao.getAllProductDTO();
+        List<Category> listC = dao.getAllCategory();
+        
+        request.setAttribute("listS", list);
+        request.setAttribute("listCC", listC);
+        
+        request.getRequestDispatcher("homePage.jsp").
+                forward(request, response);
+        //processRequest(request, response);
     }
 
     /**
