18bc1d241862651e1bff760c884fc1715139f19d
dt2401 <quanphse161574@fpt.edu.vn>
2023-10-26 00:06:36 +0700

    add orderdetail model

---
+++ src/java/model/OrderDetail.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author trand
+ */
+public class OrderDetail {
+    String cageName;
+    int quantity;
+    double price;
+    String imageUrl;
+
+    public OrderDetail() {
+    }
+
+    public OrderDetail(String cageName, int quantity, double price, String imageUrl) {
+        this.cageName = cageName;
+        this.quantity = quantity;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getCageName() {
+        return cageName;
+    }
+
+    public void setCageName(String cageName) {
+        this.cageName = cageName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    
+    
+    
+}
