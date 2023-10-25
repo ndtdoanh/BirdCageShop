ad35439ba1e418a2eefcf5fe659fa4934db98499
tvmmanh/ADMIN <tivip1216vn@gmail.com>
2023-10-25 23:54:48 +0700

    wishlist model

---
+++ src/java/model/Wishlist.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author trand
+ */
+public class Wishlist {
+    String cageId;
+    String image;
+    String cageName;
+    double price;
+    String status;
+
+    public Wishlist() {
+    }
+
+    public Wishlist(String cageId, String image, String cageName, double price, String status) {
+        this.cageId = cageId;
+        this.image = image;
+        this.cageName = cageName;
+        this.price = price;
+        this.status = status;
+    }
+
+    public String getCageId() {
+        return cageId;
+    }
+
+    public void setCageId(String cageId) {
+        this.cageId = cageId;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCageName() {
+        return cageName;
+    }
+
+    public void setCageName(String cageName) {
+        this.cageName = cageName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    } 
+}
