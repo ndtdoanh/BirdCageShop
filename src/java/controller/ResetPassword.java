60e81d2fa0dfc2b18b2bcf145cbfc31825843149
TRONGDOANH\Acer <nguyentrongdoanh@gmail.com>
2023-11-04 01:13:55 +0700

    update control dẫn policy

---
+++ src/java/controller/ResetPassword.java
@@ -0,0 +1,110 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dao.UserDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author trand
+ */
+@WebServlet(name = "ResetPassword", urlPatterns = {"/ResetPassword"})
+public class ResetPassword extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ResetPassword</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ResetPassword at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        String code = request.getParameter("code");
+        String codeValid = (String)request.getSession().getAttribute("code");
+        if(code.equals(codeValid)){
+            request.getSession().removeAttribute("code");
+            request.getRequestDispatcher("ResetPassword.jsp").forward(request, response);
+        }else{
+            request.setAttribute("ERROR", "Mã xác nhận không đúng");
+            request.getRequestDispatcher("OtpPassword.jsp").forward(request, response);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        String password = request.getParameter("newPassword");
+        String validPassword = request.getParameter("validPassword");
+        String email = (String)request.getSession().getAttribute("email");
+        if(password.equals(validPassword)){
+            UserDAO ud = new UserDAO();
+            ud.updatePasswordByEmail(password, email);
+            request.getSession().removeAttribute("email");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+        }else{
+            request.setAttribute("ERROR", "Mật khẩu không trùng khớp");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
